08d0dcb58f19bf0da6e1a758d3443ebadf92fb43
Petri Varvia <petri.varvia@gmail.com>
2023-09-04 11:19:35 +0300

    Update test-mgpr.R

--- tests/testthat/test-mgpr.R
+++ tests/testthat/test-mgpr.R
@@ -62,3 +62,39 @@ test_that("mgpr object is correct (multivariate)", {
   #K diagonal elements should be sigma^2
   expect_setequal(diag(m$K),1)
 })
+
+test_that("mean functions work", {
+  x <- c(0,1,2)
+  y <- x
+  #zero
+  m1 <- mgpr(datay = y, datax = x,kernel = "rbf", meanf = "zero",
+            kernpar = list(sigma = 1, corlen = 1, errorvar = 0.1))
+  expect_equal(m1$meanf(10),0)
+  #avg
+  m2 <- mgpr(datay = y, datax = x,kernel = "rbf", meanf = "avg",
+             kernpar = list(sigma = 1, corlen = 1, errorvar = 0.1))
+  expect_equal(as.numeric(m2$meanf(10)),mean(y))
+  #linear
+  m3 <- mgpr(datay = y, datax = x,kernel = "rbf", meanf = "linear",
+             kernpar = list(sigma = 1, corlen = 1, errorvar = 0.1))
+  #x is standardized to zero mean, unit variance
+  expect_equal(as.numeric(m3$meanf((10-mean(x))/sd(x))),10)
+})
+
+test_that("covariance functions work", {
+  #rbf
+  x <- c(0,1,2)
+  y <- x
+  m1 <- mgpr(datay = y, datax = x,kernel = "rbf", meanf = "zero",
+             kernpar = list(sigma = 1, corlen = 1, errorvar = 0.1))
+  expect_equal(m1$covfunc(1,1,1),exp(-0.5))
+  m2 <- mgpr(datay = y, datax = x,kernel = "matern12", meanf = "zero",
+             kernpar = list(sigma = 1, corlen = 1, errorvar = 0.1))
+  expect_equal(m2$covfunc(1,1,1),exp(-1))
+  m3 <- mgpr(datay = y, datax = x,kernel = "matern32", meanf = "zero",
+             kernpar = list(sigma = 1, corlen = 1, errorvar = 0.1))
+  expect_equal(m3$covfunc(1,1,1),(1 + sqrt(3)) * exp(-sqrt(3)))
+  m4 <- mgpr(datay = y, datax = x,kernel = "matern52", meanf = "zero",
+             kernpar = list(sigma = 1, corlen = 1, errorvar = 0.1))
+  expect_equal(m4$covfunc(1,1,1),(1 + sqrt(5) + 5/3) * exp(-sqrt(5)))
+})
